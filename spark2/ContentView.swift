b9bab41ceed6744584231d446cbb8f15385f4d90
rtshah <rahultshah@gmail.com>
2024-03-17 22:14:52 -0500

    Initial Commit

---
+++ spark2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spark2
+//
+//  Created by Rahul Shah on 3/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spark2/spark2App.swift
@@ -0,0 +1,17 @@
+//
+//  spark2App.swift
+//  spark2
+//
+//  Created by Rahul Shah on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct spark2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
