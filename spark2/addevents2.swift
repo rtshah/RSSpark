cd56e8449ba90bb29c09d881e7941cc070697f3c
rtshah <rahultshah@gmail.com>
2024-04-07 23:54:43 -0500

    tweaks

--- spark2/addevents2.swift
+++ spark2/addevents2.swift
@@ -1,5 +1,14 @@
 import SwiftUI
 
+extension DateFormatter {
+    static let timeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .short
+        return formatter
+    }()
+}
+
 struct AddEvents2: View {
     @State private var eventName: String = ""
     @State private var eventDescription: String = ""
@@ -10,8 +19,9 @@ struct AddEvents2: View {
     @State private var friendsOnlyText: String = "Friends Only"
     @State private var isShared = false
     @State private var userInput: String = ""
+    @ObservedObject var viewModel = EventDateTimeViewModel()
     
-
+    
     var body: some View {
         ZStack {
             VStack(alignment: .leading, spacing: 32.5) {
@@ -25,59 +35,115 @@ struct AddEvents2: View {
                     Spacer()
                 }
                 .padding(.top, 16)
-
+                                
                 TextField("Event Name", text: $eventName)
                     .padding()
-                    .frame(width: 205, height: 38)
                     .background(Color.gray)
                     .cornerRadius(10)
                     .foregroundColor(.white)
-                    .padding(.horizontal, 48)
-                    .multilineTextAlignment(.leading)
-
-                Text("Your link:")
-                    .font(.system(size: 17))
-                    .bold()
+                
+                TextField("Theme, description, etc!", text: $eventDescription)
+                    .padding(32)
+                    .background(Color.gray)
+                    .cornerRadius(10)
                     .foregroundColor(.white)
 
-                HStack {
-                    Text("spark.sampleurl.123456789")
-                        .font(.system(size: 17))
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.leading)
-                        .padding(.trailing, 60)
-                    
-                    ZStack {
-                        VStack {
-                            Button(action: {
-                                withAnimation {
-                                    self.isShared.toggle()
-                                }
-                            }) {
-                                Image(systemName: "square.and.arrow.up")
-                                    .font(.system(size: 24))
-                                    .foregroundColor(.white)
-                            }
+                TextField("Location", text: $location)
+                    .padding()
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                
+                if viewModel.timeHasBeenSet {
+                    VStack {
+                        Text("Starts at \(viewModel.startTime, formatter: DateFormatter.timeFormatter)")
+                            .foregroundColor(.white)
+                        Button("Change") {
+                            viewModel.isShowingSetTimeView = true
+                        }
+                        .font(.system(size: 12))
+                        .foregroundColor(.blue)
+                        .padding(.horizontal, 48)
+                    }
+                } else {
+                    Button("Set Time") {
+                        viewModel.isShowingSetTimeView = true
+                    }
+                    .foregroundColor(.black)
+                    .bold()
+                    .frame(width: 150, height: 50)
+                    .background(Color.white)
+                    .cornerRadius(30)
+                    .multilineTextAlignment(.center)
+                }
+                
+                Text("Who's coming?")
+                    .font(.system(size: 20))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 48)
+                VStack(alignment: .leading, spacing: 15) {
+                    Button(action: {
+                        selection = "Everyone"
+                        everyoneText = "Let's Rage 🎉"
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            everyoneText = "Everyone"
+                        }
+                    }) {
+                        HStack {
+                            Image(systemName: selection == "Everyone" ? "largecircle.fill.circle" : "circle")
+                                .foregroundColor(.white)
+                            Text(everyoneText)
+                                .font(.system(size: 17))
+                        
+                                .foregroundColor(.white)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                         }
                     }
+                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal, 48)
+                    
+                    Button(action: {
+                        selection = "Friends and Mutuals Only"
+                        friendsAndMutualsText = "Kickback 🤗"
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            friendsAndMutualsText = "Friends and Mutuals Only"
+                        }
+                    }) {
+                        HStack {
+                            Image(systemName: selection == "Friends and Mutuals Only" ? "largecircle.fill.circle" : "circle")
+                                .foregroundColor(.white)
+                            Text(friendsAndMutualsText)
+                                .font(.system(size: 17))
+                                .foregroundColor(.white)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal, 48)
+
+                    Button(action: {
+                        selection = "Friends Only"
+                        friendsOnlyText = "Small hangout 😄"
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            friendsOnlyText = "Friends Only"
+                        }
+                    }) {
+                        HStack {
+                            Image(systemName: selection == "Friends Only" ? "largecircle.fill.circle" : "circle")
+                                .foregroundColor(.white)
+                            Text(friendsOnlyText)
+                                .font(.system(size: 17))
+                                .foregroundColor(.white)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal, 48)
+
                 }
 
-                Text("Your SparkID:")
-                    .font(.system(size: 17))
-                    .bold()
-                    .foregroundColor(.white)
-
-                Text("samplespa")
-                    .font(.system(size: 17))
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.leading)
-                    .padding(.trailing, 60)
                 
-                Text("Add a SparkID:")
-                    .font(.system(size: 17))
-                    .bold()
-                    .foregroundColor(.white)
-
                 HStack {
                     TextField("Optional", text: $userInput)
                         .foregroundColor(.black)
@@ -85,17 +151,17 @@ struct AddEvents2: View {
                         .background(.gray)
                         .cornerRadius(20)
                 }
-
+                
                 Button(action: {}) {
                     Text("Continue")
                         .font(.system(size: 17, weight: .bold))
                         .foregroundColor(.black)
                 }
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical, 15)
-                        .background(.white)
-                        .cornerRadius(40)
-                        .padding(.top, 25)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 15)
+                .background(.white)
+                .cornerRadius(40)
+                .padding(.top, 25)
             }
             .padding(.horizontal, 16)
         }
